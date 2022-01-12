c1c1d7bc4027d1345bf68bb0e34f12208b02f950
unknown <priyambi.hiran@gmail.com>
2022-01-12 10:27:09 +0530

    Hangman Game Added

--- Projects/Hangman.py
+++ Projects/Hangman.py
@@ -15,3 +15,21 @@ elif choice=="Subjects":
     word=random.choice(Subjects)
 elif choice=="ShoppingBrands":
     word=random.choice(Shopping_Brands)
+n=len(word)
+# print(word)
+# print(n)
+print("Your word to guess is : ")
+ans="_ "*n
+print("_ "*n)
+while n!=0 :
+    char=input(("Guess a letter: "))
+    if word.find(char) != -1:
+        n=n-1
+        i=word.index(char)
+        print(i)
+        i=i*2
+        ans = ans[:i] + char + ans[i+1:]
+        print(ans)
+    else:
+        print("Wrong choice, Guess another character ")
+
